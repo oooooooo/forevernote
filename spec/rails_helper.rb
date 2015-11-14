8bf8b8b60ca587cea549c3a7f14b0a71a8cfb4f0
oooooooo <bluecatbluedog@hotmail.com>
2015-11-15 00:15:24 +0900

    Fix WebMock::NetConnectNotAllowedError

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ require 'spec_helper'
 require 'rspec/rails'
 require 'codeclimate-test-reporter'
 CodeClimate::TestReporter.start
+WebMock.disable_net_connect!(allow: 'codeclimate.com')
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
