9d767f192f6948efe30cc4a5a2f071c302170a6f
oooooooo <bluecatbluedog@hotmail.com>
2015-11-16 22:37:02 +0900

    Reindex after commit

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,12 @@
 class Note < ActiveRecord::Base
   searchkick
 
+  after_commit :reindex_note
+
   validates :title, length: { maximum: 200 }, presence: true
   validates :body, length: { maximum: 1000 * 80 }, presence: true
+
+  def reindex_note
+    reindex
+  end
 end
