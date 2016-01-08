c667951464b239a213e8d07d4ba68dc4b127871a
oooooooo <bluecatbluedog@hotmail.com>
2016-01-08 15:21:23 +0900

    I don't need a title

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,7 +3,6 @@ class Note < ActiveRecord::Base
 
   after_commit :reindex_note
 
-  validates :title, length: { maximum: 200 }, presence: true
   validates :body, length: { maximum: 1000 * 80 }, presence: true
 
   def reindex_note
