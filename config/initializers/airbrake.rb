78f0a58646c280577dd5447341aa473fce38721d
oooooooo <bluecatbluedog@hotmail.com>
2016-01-08 12:09:44 +0900

    run if you don't use airbrake

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,7 +1,9 @@
 require 'socket'
-Airbrake.configure do |config|
-  config.api_key = ENV['AIRBRAKE_PROJECT_API_KEY']
-  config.host    = ENV['HOSTNAME'] || Socket.gethostname # request.host doesn't work here
-  config.port    = 80
-  config.secure  = config.port == 443
+if ENV['AIRBRAKE_PROJECT_API_KEY']
+  Airbrake.configure do |config|
+    config.api_key = ENV['AIRBRAKE_PROJECT_API_KEY']
+    config.host    = ENV['HOSTNAME'] || Socket.gethostname # request.host doesn't work here
+    config.port    = 80
+    config.secure  = config.port == 443
+  end
 end
